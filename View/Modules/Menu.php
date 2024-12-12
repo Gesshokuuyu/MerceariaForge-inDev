7c632152dc1a8ee112910e09571cde79be833319
ForgeSolucoesEstagio <andreilucianodietrich@gmail.com>
2024-12-12 14:50:44 -0300

    Inicio home e modulos

---
+++ View/Modules/Menu.php
@@ -0,0 +1,17 @@
+<nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <!-- Inicio Botao Menu -->
+        <button class="btn btn-outline-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="bi bi-list"></i></button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasRightLabel">Menu</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            ...
+          </div>
+          <!-- fim botao menu -->
+    </a>
+  </div>
+</nav>
