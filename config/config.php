ceba10e914e7488aed60069062c9c05b6098cf02
ForgeSolucoesEstagio <169064248+Gesshokuuyu@users.noreply.github.com>
2024-12-27 16:50:25 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,12 +3,15 @@ class Database {
     protected $pdo;
 
     public function __construct() {
-        $host = '127.0.0.1'; // Ou 'localhost'
+        $host = '127.0.0.1';
         $db   = 'MFG';
         $user = 'root';
         $pass = '';
         $charset = 'utf8';
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $dsn = "mysql:
+                host=$host;
+                dbname=$db;
+                charset=$charset";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
